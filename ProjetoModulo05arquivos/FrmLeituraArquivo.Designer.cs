7dc929c53e098863ca651d52b459cdbce24d2784
Lucasedu191 <lucasatletico@hotmail.com>
2018-09-02 23:29:19 -0300

    adicionando mais duas formas para fazer a leitura de documentos

--- ProjetoModulo05arquivos/FrmLeituraArquivo.Designer.cs
+++ ProjetoModulo05arquivos/FrmLeituraArquivo.Designer.cs
@@ -31,11 +31,12 @@
             this.btnLeitura = new System.Windows.Forms.Button();
             this.lsbConteudo = new System.Windows.Forms.ListBox();
             this.btnLeitura2 = new System.Windows.Forms.Button();
+            this.btnLeitura3 = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // btnLeitura
             // 
-            this.btnLeitura.Location = new System.Drawing.Point(54, 27);
+            this.btnLeitura.Location = new System.Drawing.Point(12, 27);
             this.btnLeitura.Name = "btnLeitura";
             this.btnLeitura.Size = new System.Drawing.Size(150, 23);
             this.btnLeitura.TabIndex = 0;
@@ -53,7 +54,7 @@
             // 
             // btnLeitura2
             // 
-            this.btnLeitura2.Location = new System.Drawing.Point(274, 27);
+            this.btnLeitura2.Location = new System.Drawing.Point(185, 27);
             this.btnLeitura2.Name = "btnLeitura2";
             this.btnLeitura2.Size = new System.Drawing.Size(150, 23);
             this.btnLeitura2.TabIndex = 2;
@@ -61,11 +62,22 @@
             this.btnLeitura2.UseVisualStyleBackColor = true;
             this.btnLeitura2.Click += new System.EventHandler(this.btnLeitura2_Click);
             // 
+            // btnLeitura3
+            // 
+            this.btnLeitura3.Location = new System.Drawing.Point(358, 27);
+            this.btnLeitura3.Name = "btnLeitura3";
+            this.btnLeitura3.Size = new System.Drawing.Size(150, 23);
+            this.btnLeitura3.TabIndex = 3;
+            this.btnLeitura3.Text = "3 - Realizar Leitura";
+            this.btnLeitura3.UseVisualStyleBackColor = true;
+            this.btnLeitura3.Click += new System.EventHandler(this.btnLeitura3_Click);
+            // 
             // FrmLeituraArquivo
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(515, 305);
+            this.Controls.Add(this.btnLeitura3);
             this.Controls.Add(this.btnLeitura2);
             this.Controls.Add(this.lsbConteudo);
             this.Controls.Add(this.btnLeitura);
@@ -80,5 +92,6 @@
         private System.Windows.Forms.Button btnLeitura;
         private System.Windows.Forms.ListBox lsbConteudo;
         private System.Windows.Forms.Button btnLeitura2;
+        private System.Windows.Forms.Button btnLeitura3;
     }
 }
--- ProjetoModulo05arquivos/FrmLeituraArquivo.cs
+++ ProjetoModulo05arquivos/FrmLeituraArquivo.cs
@@ -25,6 +25,7 @@ namespace ProjetoModulo05arquivos
             //se o arquivo ja existir aparece a mensagem se não ele aparece outra mensagem e realiza a criaçao
             if (File.Exists(nomeArq))
             {
+                //realiza a leitura de todo o arquivo apenas uma vez
                 string[] array = File.ReadAllLines(nomeArq);
                 lsbConteudo.Items.Clear();
                 foreach (var item in array)
@@ -42,14 +43,41 @@ namespace ProjetoModulo05arquivos
             //se o arquivo ja existir aparece a mensagem se não ele aparece outra mensagem e realiza a criaçao
             if (File.Exists(nomeArq))
             {
+                //realiza a leitura linha a linha do documento 
                 StreamReader reader = new StreamReader(nomeArq);
-                string linha;
-                while((linha = reader.ReadLine()) != null)
+                string linha = reader.ReadLine();
+                lsbConteudo.Items.Clear();
+                while(linha != null)
                 {
                     lsbConteudo.Items.Add(linha);
+                    linha = reader.ReadLine();
+
                 }
                 reader.Close();
             }
+        }
+
+        private void btnLeitura3_Click(object sender, EventArgs e)
+        {
+            //variavel para o nome do arquivo que sera criado
+            //para reconhecer o local deve-se colocar um @ antes ou colocar \\
+            string nomeArq = @"C:\Users\adm\Documents\Visual Studio 2017\exercicios\Curso Forms\ProjetoModulo05arquivos\ProjetoModulo05arquivos\bin\Debug\Arquivos\Arquivo.txt";
+            //se o arquivo ja existir aparece a mensagem se não ele aparece outra mensagem e realiza a criaçao
+            if (File.Exists(nomeArq))
+            {
+                //realiza a leitura linha a linha do documento 
+                StreamReader reader = File.OpenText(nomeArq);
+                lsbConteudo.Items.Clear();
+                while (!reader.EndOfStream)
+                {
+                    string linha = reader.ReadLine();
+                    lsbConteudo.Items.Add(linha);
+                }
+                
+                reader.Close();
+            }
+
+
         }
     }
 }
